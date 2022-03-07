4200243f99c0860d08aac330a4c161821e297b68
dhkstnaos <dhkstnaos@gmail.com>
2022-03-07 16:52:57 +0900

    Add Swagger Setting

--- src/main/java/com/ireland/ager/board/controller/CommentController.java
+++ src/main/java/com/ireland/ager/board/controller/CommentController.java
@@ -8,6 +8,8 @@ import com.ireland.ager.board.service.CommentServiceImpl;
 import com.ireland.ager.main.common.CommonResult;
 import com.ireland.ager.main.common.SingleResult;
 import com.ireland.ager.main.common.service.ResponseService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,10 +37,15 @@ public class CommentController {
      * @Parameter : [accessToken, commentRequest, boardId]
      * @Return : ResponseEntity<SingleResult<CommentResponse>>
      **/
+    @ApiOperation(value="댓글 등록")
     @PostMapping("/{boardId}/comments")
-    public ResponseEntity<SingleResult<CommentResponse>> createComment(@RequestHeader("Authorization") String accessToken,
-                                                                       @RequestPart(value = "comments") CommentRequest commentRequest,
-                                                                       @PathVariable(value = "boardId") Long boardId) throws IOException {
+    public ResponseEntity<SingleResult<CommentResponse>> createComment(
+            @ApiParam(value = "액세스 토큰", required = true)
+            @RequestHeader("Authorization") String accessToken,
+            @ApiParam(value = "댓글 DTO", required = true)
+            @RequestPart(value = "comments") CommentRequest commentRequest,
+            @ApiParam(value = "boardId", required = true)
+            @PathVariable(value = "boardId") Long boardId) throws IOException {
 
         String[] splitToken = accessToken.split(" ");
         CommentResponse commentResponse = commentService.saveComment(splitToken[1], boardId, commentRequest);
@@ -51,12 +58,17 @@ public class CommentController {
      * @Parameter : [accessToken, commentRequest, boardId, commentId]
      * @Return : ResponseEntity<SingleResult<CommentResponse>>
      **/
+    @ApiOperation(value="댓글 수정")
     @PatchMapping("/{boardId}/comments/{commentId}")
-    public ResponseEntity<SingleResult<CommentResponse>> updateComment(@RequestHeader("Authorization") String accessToken,
-                                                                       @RequestPart(value = "comments") CommentRequest commentRequest,
-                                                                       @PathVariable(value = "boardId") Long boardId,
-                                                                       @PathVariable(value = "commentId") Long commentId) throws IOException {
-
+    public ResponseEntity<SingleResult<CommentResponse>> updateComment(
+            @ApiParam(value = "액세스 토큰", required = true)
+            @RequestHeader("Authorization") String accessToken,
+            @ApiParam(value = "댓글 DTO", required = true)
+            @RequestPart(value = "comments") CommentRequest commentRequest,
+            @ApiParam(value = "boardId", required = true)
+            @PathVariable(value = "boardId") Long boardId,
+            @ApiParam(value = "commentId", required = true)
+            @PathVariable(value = "commentId") Long commentId) throws IOException {
         String[] splitToken = accessToken.split(" ");
         CommentResponse commentResponse = commentService.updateComment(splitToken[1], commentId, commentRequest);
         return new ResponseEntity<>(responseService.getSingleResult(commentResponse), HttpStatus.OK);
@@ -68,10 +80,15 @@ public class CommentController {
      * @Parameter : [accessToken, boardId, commentId]
      * @Return : ResponseEntity<CommonResult>
      **/
+    @ApiOperation(value="댓글 삭제")
     @DeleteMapping("/{boardId}/comments/{commentId}")
-    public ResponseEntity<CommonResult> deleteComment(@RequestHeader("Authorization") String accessToken,
-                                                      @PathVariable(value = "boardId") Long boardId,
-                                                      @PathVariable(value = "commentId") Long commentId) throws IOException {
+    public ResponseEntity<CommonResult> deleteComment(
+            @ApiParam(value = "액세스 토큰", required = true)
+            @RequestHeader("Authorization") String accessToken,
+            @ApiParam(value = "boardId", required = true)
+            @PathVariable(value = "boardId") Long boardId,
+            @ApiParam(value = "commentId", required = true)
+            @PathVariable(value = "commentId") Long commentId) throws IOException {
         String[] splitToken = accessToken.split(" ");
         commentService.deleteComment(splitToken[1], commentId);
         return new ResponseEntity<>(responseService.getSuccessResult(), HttpStatus.OK);

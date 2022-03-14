3625b11edb5315b1e08490a81375fedc2a97f669
jhdl0157 <jhdl0157@naver.com>
2022-03-14 12:57:08 +0900

    feat: Add RoutingDataSource

---
+++ src/main/java/com/ireland/ager/config/RoutingDataSource.java
@@ -0,0 +1,20 @@
+package com.ireland.ager.config;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+public class RoutingDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+        if (isReadOnly) {
+            logger.info("Connection Slave");
+            return "slave";
+        } else {
+            logger.info("Connection Master");
+            return "master";
+        }
+    }
+}
+
